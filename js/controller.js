c5a9273bcecee6e7006418080a975ce044e4b8a4
Andrew Cer <cer.andrew@gmail.com>
2015-08-26 13:13:40 -0600

    conversion works for dollar to btc xdg and ltc

--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,30 @@
 app.controller('CurrencyController', ['$scope', '$firebaseObject', function ($scope, $firebaseObject) {
-  var bitcoinRef = new Firebase('https://publicdata-cryptocurrency.firebaseio.com/bitcoin');
+  var bitcoinRef = new Firebase('https://publicdata-cryptocurrency.firebaseio.com');
+  console.log($firebaseObject(bitcoinRef));
   $scope.bitcoinData = $firebaseObject(bitcoinRef)
+  $scope.currencyChanger = function (input) {
+    if (!input) {
+      $scope.dogey = false;
+      $scope.litey = false;
+      $scope.bitty = false;
+    }
+    if (input === 'bitcoin') {
+      $scope.chosenCurrency = 'BTC'
+      $scope.dogey = false;
+      $scope.litey = false;
+      $scope.bitty = true;
+    }
+    if (input === 'dogecoin') {
+      $scope.chosenCurrency = 'XDG'
+      $scope.litey = false;
+      $scope.bitty = false;
+      $scope.dogey = true;
+    }
+    if (input === 'litecoin') {
+      $scope.chosenCurrency = 'LTC'
+      $scope.bitty = false;
+      $scope.dogey = false;
+      $scope.litey = true;
+    }
+  }
 }])
